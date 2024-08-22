f1b8154d03826ed8c1d0f4142e652a9a15800dd5
BadDust80131 <baddust80131@gmail.com>
2024-08-22 15:43:06 -0700

    First Webpack Complete

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
+
+
+// use npx webpack, to pack everything
+// use node to run main.js file
